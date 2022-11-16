6f28e7992da29b90d0832e79d24249ef2c942d4d
renersullivan1 <renersullivan1@gmail.com>
2022-11-15 23:38:11 -0300

    criando json do back

--- src/app/pages/login/view/login/login.page.ts
+++ src/app/pages/login/view/login/login.page.ts
@@ -7,15 +7,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  atuh: {
-    login: 'rener';
-    senha: '172018';
-  };
-  constructor(private httpclient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {}
-
   fazerLogin() {
-    this.httpclient.get('http://127.0.0.1:3333/').subscribe=>{}};
+    // alert('clicou no btn');
+    const auth = {
+      login: 'rener',
+      senha: '172018',
+    };
+    console.log(auth);
+    this.httpClient
+      .post<any>('http://127.0.0.1:3333/', auth)
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }
